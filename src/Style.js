64879bf7c4d0a753d3cbfb6e5e30daedc7308875
Shabin-k <helloshabink@gmail.com>
2023-04-06 18:39:11 +0530

    fixed dark

--- src/Style.js
+++ src/Style.js
@@ -1,5 +1,5 @@
 const styles = {
-    navUserMenu : "block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white",
+    navUserMenu : "block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600 hover:text-white",
     sideUserMenu : "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700",
     sideUserMenuSvg : "flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
 
