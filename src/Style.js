04208cbfbcc3ce74c959229d143c9bf72f4f3b7d
Shabin-k <helloshabink@gmail.com>
2023-04-28 10:36:33 +0530

    ....first commit

--- src/Style.js
+++
@@ -1,7 +0,0 @@
-const styles = {
-    navUserMenu : "block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600 hover:text-white",
-    sideUserMenu : "flex items-center p-2  rounded-lg text-white hover:bg-gray-700",
-    sideUserMenuSvg : "flex-shrink-0 w-6 h-6  transition duration-75 text-gray-400 group-hover:text-white"
-  };
-  
-  export default styles;
