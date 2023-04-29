4f530cab99fab34df2b92b6af1198b6f2d3ac26d
Shabin-k <helloshabink@gmail.com>
2023-04-29 11:25:06 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Components/User/Header';
 
 function App() {
 
   return (
     <div className="bg-gray-100">
-        hel
+        <Header/>
     </div>
   );
 }
