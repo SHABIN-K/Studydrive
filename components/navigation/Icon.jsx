8d86db3f1b3c981d371fdb123cf2c504a3518ff4
Shabin-k <helloshabink@gmail.com>
2023-07-16 21:26:27 +0530

    feat : create sidebar

---
+++ components/navigation/Icon.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const Icon = ({ styles, name, imgUrl, isActive, handleClick }) => {
+    return (
+      <div
+        className={`w-[48px] h-[48px] rounded-[10px] ${
+          isActive && isActive === name && "bg-[#2c2f32]"
+        } flex justify-center items-center cursor-pointer"
+         ${styles}`}
+        onClick={handleClick}
+      >
+        {!isActive ? (
+          <Image src={imgUrl} alt="aside_icons" className="w-1/2 h-1/2" />
+        ) : (
+          <Image
+            src={imgUrl}
+            alt="aside_icons"
+            className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+          />
+        )}
+      </div>
+    );
+  };
+
+  export default Icon;
--- components/navigation/Sidebar.jsx
+++ components/navigation/Sidebar.jsx
@@ -1,9 +1,44 @@
-'use client';
+"use client";
+
+import { useState } from "react";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { navlinks } from "@/constants";
+import { logo, sun } from "@/public/assets";
+import Icon from "./Icon";
 
 const Sidebar = () => {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState("home");
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className="flex justify-between items-center flex-col sticky top-5 h-[89vh]">
+      <Link href="/">
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+      </Link>
 
-export default Sidebar
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+        <div className="flex flex-col justify-center items-center gap-3">
+          {navlinks.map((data) => {
+            return (
+              <div key={data.name}>
+                <Icon
+                  {...data}
+                  isActive={isActive}
+                  handleClick={() => {
+                    setIsActive(data.name);
+                    router.push(data.link);
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <Icon className="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
