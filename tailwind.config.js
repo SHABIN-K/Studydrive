129c09cdeac186c752ef536972ef42210927a3be
Shabin-k <helloshabink@gmail.com>
2023-07-16 20:14:10 +0530

    feat: add: add secondary shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+      boxShadow: {
+        secondary: "10px 10px 20px rgba(2, 2, 2, 0.25)",
+      },
+    },
+  },
   plugins: [],
-}
+};
