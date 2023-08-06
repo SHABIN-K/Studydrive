857f9382714a86e0f91954d8ba7e5f93728bb87c
Shabin-k <helloshabink@gmail.com>
2023-08-06 12:42:47 +0530

    fix: font

--- components/navigation/Navbar.jsx
+++ components/navigation/Navbar.jsx
@@ -3,9 +3,9 @@
 
 import { useEffect, useRef, useState } from "react";
 
-import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+
 import { navlinks } from "@/constants";
 import { close, logo, menu, search } from "@/public/assets";
 
@@ -44,108 +44,93 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Tektur:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
+    <nav className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+        <input
+          type="text"
+          placeholder="Search for Study materials"
+          className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
         />
-      </Head>
-      <nav className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-        <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
-          <input
-            type="text"
-            placeholder="Search for Study materials"
-            className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <Image
+            src={search}
+            alt="search icon"
+            className="w-[15px] h-[15px] object-contain"
           />
-          <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
-            <Image
-              src={search}
-              alt="search icon"
-              className="w-[15px] h-[15px] object-contain"
-            />
-          </div>
+        </div>
+      </div>
+
+      {/* Small screen navigation */}
+
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div
+          className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          <Image
+            src={logo}
+            alt="user icon"
+            className="w-[60%] h-[60%] object-contain"
+          />
         </div>
 
-        {/* Small screen navigation */}
+        <p className="text-[#4acd8d] align-middle text-center subpixel-antialiased text-2xl">
+          PASC HUB
+        </p>
 
-        <div className="sm:hidden flex justify-between items-center relative">
-          <div
-            className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer"
-            onClick={() => router.push("/")}
-          >
-            <Image
-              src={logo}
-              alt="user icon"
-              className="w-[60%] h-[60%] object-contain"
-            />
-          </div>
+        <div
+          className={`w-[34px] h-[34px] object-contain cursor-pointer transition-transform transform ${
+            toggleDrawer ? "rotate-45" : "rotate-0"
+          }`}
+          onClick={handleToggleDrawer}
+          ref={menuButtonRef}
+        >
+          <Image
+            src={toggleDrawer ? close : menu}
+            alt="menu icon"
+            className="w-full h-full"
+          />
+        </div>
 
-          <p
-            className="text-[#4acd8d] align-middle text-center subpixel-antialiased text-2xl"
-            style={{ fontFamily: "Tektur, cursive", fontWeight: 400 }}
-          >
-            PASC HUB
-          </p>
-
-          <div
-            className={`w-[34px] h-[34px] object-contain cursor-pointer transition-transform transform ${
-              toggleDrawer ? "rotate-45" : "rotate-0"
-            }`}
-            onClick={handleToggleDrawer}
-            ref={menuButtonRef}
-          >
-            <Image
-              src={toggleDrawer ? close : menu}
-              alt="menu icon"
-              className="w-full h-full"
-            />
-          </div>
-
-          <div
-            className={`${
-              toggleDrawer ? "translate-x-0" : "-translate-x-full"
-            } fixed top-0 bottom-0 left-0 rounded-r-[10px] bg-[#1c1c24] z-10 shadow-secondary py-5 w-[250px] transition-transform duration-1000`}
-            ref={sidebarRef}
-          >
-            <ul className="mb-4 p-3">
-              {navlinks.map((data) => (
-                <li
-                  key={data.name}
-                  className={`flex p-4 ${
-                    isActive === data.name && "bg-[#3a3a43] rounded-full"
+        <div
+          className={`${
+            toggleDrawer ? "translate-x-0" : "-translate-x-full"
+          } fixed top-0 bottom-0 left-0 rounded-r-[10px] bg-[#1c1c24] z-10 shadow-secondary py-5 w-[250px] transition-transform duration-1000`}
+          ref={sidebarRef}
+        >
+          <ul className="mb-4 p-3">
+            {navlinks.map((data) => (
+              <li
+                key={data.name}
+                className={`flex p-4 ${
+                  isActive === data.name && "bg-[#3a3a43] rounded-full"
+                }`}
+                onClick={() => {
+                  setIsActive(data.name);
+                  setToggleDrawer(false);
+                  router.push(data.link);
+                }}
+              >
+                <Image
+                  src={data.imgUrl}
+                  alt={data.name}
+                  className={`w-[24px] h-[24px] object-contain ${
+                    isActive === data.name ? "grayscale-0" : "grayscale"
                   }`}
-                  onClick={() => {
-                    setIsActive(data.name);
-                    setToggleDrawer(false);
-                    router.push(data.link);
-                  }}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === data.name ? "text-[#1dc071]" : "text-[#808191]"
+                  }`}
                 >
-                  <Image
-                    src={data.imgUrl}
-                    alt={data.name}
-                    className={`w-[24px] h-[24px] object-contain ${
-                      isActive === data.name ? "grayscale-0" : "grayscale"
-                    }`}
-                  />
-                  <p
-                    className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
-                      isActive === data.name
-                        ? "text-[#1dc071]"
-                        : "text-[#808191]"
-                    }`}
-                  >
-                    {data.name}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  {data.name}
+                </p>
+              </li>
+            ))}
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
