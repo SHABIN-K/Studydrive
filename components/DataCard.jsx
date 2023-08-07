f689ca9a5bf6c2a3ba7294afa9e920b4f96ac652
Shabin-k <helloshabink@gmail.com>
2023-08-07 16:08:12 +0530

    feat : cpl catergories card

---
+++ components/DataCard.jsx
@@ -0,0 +1,24 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import img from "@/public/test.png";
+
+const DataCard = ({ data }) => {
+  const router = useRouter();
+  return (
+    <div
+      className="cursor-pointer flex flex-col items-center"
+      onClick={() => router.push(data.link)}
+    >
+      <div className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
+        <Image src={img} objectFit="cover" alt={data.descrption} />
+      </div>
+      <p className="text-[#808191] text-base font-medium md:font-semibold font-sans md:font-mono tracking-tighter mt-2">
+        {data.name}
+      </p>
+    </div>
+  );
+};
+
+export default DataCard;
--- components/category/Category.jsx
+++ components/category/Category.jsx
@@ -1,15 +1,15 @@
 import { category } from "@/constants/category";
-import CategoryCard from "./CategoryCard";
+import DataCard from "../DataCard";
 
 const Category = () => {
   return (
     <div>
-      <h1 className="font-epilogue font-bold sm:font-semibold text-[20px] text-white text-left">
+      <h1 className="font-epilogue font-bold sm:font-semibold text-[20px] text-green-300 text-left">
         Discover cateogries
       </h1>
-      <div className="flex flex-wrap mt-[20px] gap-[26px]">
-        {category.map((data) => {
-          return <CategoryCard key={data.id} data={data} />;
+      <div className="flex flex-wrap mt-[18px] xs:gap-1.5 md:gap-[26px] justify-between md:justify-normal">
+        {category.map((category) => {
+          return <DataCard key={category.id} data={category} />;
         })}
       </div>
     </div>
