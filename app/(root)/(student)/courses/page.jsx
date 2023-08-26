b306cba90a35943419c89e82f7ae7a880aaa67d1
Shabin-k <helloshabink@gmail.com>
2023-08-26 17:46:03 +0530

    create some bugs 😎🥳

--- app/(root)/(student)/courses/page.jsx
+++ app/(root)/(student)/courses/page.jsx
@@ -7,10 +7,12 @@ const Mycourses = () => {
       <h1 className="font-epilogue font-bold sm:font-semibold text-[20px] text-green-300 text-left">
         Select Category
       </h1>
-      <div className="flex flex-col mt-[18px]">
-        {category.map((category, index) => {
-          return <DataCard key={index} data={category} route="category" />;
-        })}
+      <div className="item-center">
+        <div className="grid grid-cols-2 mt-[18px] gap-[26px]">
+          {category.map((category, index) => {
+            return <DataCard key={index} data={category} route="semester" />;
+          })}
+        </div>
       </div>
     </div>
   );
--- components/DataCard.jsx
+++ components/DataCard.jsx
@@ -3,21 +3,22 @@ import Image from "next/image";
 
 import img from "public/icons/test.png";
 
-const DataCard = ({ data, route, style }) => {
+const DataCard = ({ data, route }) => {
   //https://daisyui.com/components/card/
   return (
-    <div className={`rounded-lg ${style}`}>
+    <div className="rounded-lg bg-[#1c1c24] hover:bg-[#2c2f32] py-2 shadow-sm shadow-gray-500">
       <Link
         href={{
-          pathname: `/${route}`,
-          query: { name: data.link },
+          pathname: `/${route}/`,
+          query: { name: data.link,  number: 1
+          },
         }}
       >
-        <div className="card cursor-pointer flex flex-wrap">
+        <div className="card cursor-pointer items-center">
           <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
             <Image src={img} alt={data.description} />
           </figure>
-          <div className="text-start">
+          <div className="items-center text-center">
             <h2 className="text-base font-medium md:font-semibold font-sans md:font-mono tracking-tighter mt-2 text-[#808191]">
               {data.name}
             </h2>
