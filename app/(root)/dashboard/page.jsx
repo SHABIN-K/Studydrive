aba72773a0a92519b5b9de759fa6884b33879a87
Shabin-k <helloshabink@gmail.com>
2023-08-27 17:01:00 +0530

    feat : change routes

--- app/(root)/dashboard/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DashBoard = () => {
-  return (
-    <div>DashBoard</div>
-  )
-}
-
-export default DashBoard
--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import "@/styles/globals.css";
+import "@/app/globals.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
