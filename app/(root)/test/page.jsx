369da1f3f7c0348aacf16dccfdfe3e5db1d97e1c
Shabin-k <helloshabink@gmail.com>
2023-09-08 20:28:30 +0530

    fix

--- app/(root)/test/page.jsx
+++ app/(root)/test/page.jsx
@@ -1,1 +1,7 @@
-////////////////////
+"use client";
+
+import { Empty } from "@/components/ui/Empty";
+
+export default function test() {
+  return <Empty />;
+}
--- components/ui/Empty.jsx
+++ components/ui/Empty.jsx
@@ -1,19 +1,23 @@
-/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 
-export const Empty = ({ label }) => {
+export const Empty = ({ label, error, reset }) => {
   return (
-    <div className="p-20 flex flex-col items-center justify-center">
-      <div className="relative h-72 w-72">
-        <Image
-          src="/empty.png"
-          alt="Empty"
-          priority
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          layout="fill" // Add this if you want to use "fill" layout
-        />
+    <section>
+      <div className="p-20 flex flex-col items-center justify-center">
+        <div className="relative h-72 w-72">
+          <Image
+            src="/empty.png"
+            alt="Empty"
+            priority
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            layout="fill" // Add this if you want to use "fill" layout
+          />
+        </div>
+        <h1 className="text-muted-foreground text-2xl text-center">{label}</h1>
+        <p className="text-muted-foreground text-sm text-center">
+          {error.message}
+        </p>
       </div>
-      <p className="text-muted-foreground text-sm text-center">{label}</p>
-    </div>
+    </section>
   );
 };
