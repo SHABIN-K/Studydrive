25e6c0b6871c792aac3427ade18c8dd3b29fdc64
Shabin-k <helloshabink@gmail.com>
2023-09-13 21:54:30 +0530

    feat : tabale

--- app/(root)/(dashboard)/(routes)/admin/page.jsx
+++ app/(root)/(dashboard)/(routes)/admin/page.jsx
@@ -1,4 +1,8 @@
+"use client";
 import { AdminHeader } from "@/components/navigation";
+import { Tab } from "@headlessui/react";
+
+const Tabs = ["Dashboard", "Users"];
 
 const AdminPanel = () => {
   return (
@@ -6,8 +10,23 @@ const AdminPanel = () => {
       <div className="flex h-screen font-normal">
         <div className="flex-1 flex flex-col overflow-hidden">
           <AdminHeader />
-          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-[#3a3a43]">
-            <div className="px-8 py-6"></div>
+          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-[#1c1c22] ">
+            <Tab.Group>
+              <Tab.List className="flex space-x-1 rounded-xl p-1   mt-2 gap-1 sm:w-2/5">
+                {Tabs.map((tab, index) => (
+                  <Tab
+                    key={index}
+                    className="w-full border-2 rounded-lg py-2.5 text-sm font-medium leading-5 text-white ring-white ring-opacity-60 ring-offset-2 "
+                  >
+                    {tab}
+                  </Tab>
+                ))}
+              </Tab.List>
+              <Tab.Panels className="mt-2">
+                <Tab.Panel>Content 1</Tab.Panel>
+                <Tab.Panel>Content 2</Tab.Panel>
+              </Tab.Panels>
+            </Tab.Group>
           </main>
         </div>
       </div>
