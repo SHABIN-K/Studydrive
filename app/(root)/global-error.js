fa8184c5e2814d7aca312e0fe584e4b6243c24ec
Shabin-k <helloshabink@gmail.com>
2023-10-13 21:06:45 +0530

    add error

--- app/(root)/global-error.js
+++ app/(root)/global-error.js
@@ -1,12 +1,13 @@
-'use client'
- 
+"use client";
+
 export default function GlobalError({ error, reset }) {
   return (
     <html>
       <body>
         <h2>Something went wrong!</h2>
+        <h3>{error}</h3>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
-  )
+  );
 }
