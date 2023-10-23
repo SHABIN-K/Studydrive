c6d8394b584e879b5b268df8fc66270d0a9a62c2
Shabin-k <helloshabink@gmail.com>
2023-10-23 16:52:46 +0530

    feat : add headers

--- app/(root)/(dashboard)/(routes)/dashboard/page.jsx
+++ app/(root)/(dashboard)/(routes)/dashboard/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Upload from "@/components/admin/Upload";
 import AdminNavbar from "@/components/admin/ui/AdminNavbar";
 import AdminSidebar from "@/components/admin/ui/AdminSidebar";
 
@@ -7,23 +8,25 @@ const MyDash = () => {
   return (
     <section className="flex h-screen font-normal">
       <div className="flex-1 flex flex-col overflow-hidden">
-        <AdminNavbar />
+        <div className="">
+          <AdminNavbar label="Dashboard" />
+        </div>
         <div className="flex flex-row">
           <div className="md:flex hidden ">
             <AdminSidebar />
           </div>
-          <main className="p-5 md:p-4 overflow-auto">
+          <main className="p-5 md:p-4 overflow-auto items-start justify-center flex-grow">
             <div className="flex flex-col items-center justify-center">
               <div className="flex-col items-center">
                 <h3 className="text-white text-3xl font-extrabold text-center">
                   Upload document
                 </h3>
-                <p className="text-gray-300 mt-1 text-sm font-extrabold text-center align-bottom mb-2 tracking-tight sm:tracking-normal">
+                <p className="text-gray-200 mt-2 text-sm font-medium text-center align-bottom mb-2">
                   Upload your summaries and other study documents to Pasc Hub
                 </p>
               </div>
-              <div className="bg-[#1d232a] p-5 rounded-2xl text-center md:min-w-[840px]">
-                Content goes here
+              <div className="bg-[#1d232a] p-3 rounded-lg text-center mt-5 border border-green-400 w-full max-h-[440px] mb-5 overflow-hidden">
+                <Upload/>
               </div>
             </div>
           </main>
@@ -34,3 +37,7 @@ const MyDash = () => {
 };
 
 export default MyDash;
+/*
+<div className="bg-[#1d232a] p-3 rounded-lg text-center mt-5 border border-green-400 ">
+content here
+</div>*/
---
+++ components/admin/Upload.jsx
@@ -0,0 +1,54 @@
+const Upload = () => {
+  return (
+    <div className="">
+      content here Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Minus rem atque quis esse fugit laudantium sed ut doloremque harum non
+      numquam, nihil, nobis quos voluptas repellat cupiditate exercitationem!
+      Corrupti, eveniet.loremlorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Neque alias odio provident? Nostrum voluptate reiciendis
+      ab qui! Ratione, eligendi delectus vel distinctio laboriosam sequi,
+      veritatis minus, excepturi quae molestiae placeat loremloreml loremlorem
+      <br />
+      content here Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Minus rem atque quis esse fugit laudantium sed ut doloremque harum non
+      numquam, nihil, nobis quos voluptas repellat cupiditate exercitationem!
+      Corrupti, eveniet.loremlorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Neque alias odio provident? Nostrum voluptate reiciendis
+      ab qui! Ratione, eligendi delectus vel distinctio laboriosam sequi,
+      veritatis minus, excepturi quae molestiae placeat loremloreml loremlorem
+      <br />
+      content here Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Minus rem atque quis esse fugit laudantium sed ut doloremque harum non
+      numquam, nihil, nobis quos voluptas repellat cupiditate exercitationem!
+      Corrupti, eveniet.loremlorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Neque alias odio provident? Nostrum voluptate reiciendis
+      ab qui! Ratione, eligendi delectus vel distinctio laboriosam sequi,
+      veritatis minus, excepturi quae molestiae placeat loremloreml loremlorem
+      content here Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Minus rem atque quis esse fugit laudantium sed ut doloremque harum non
+      numquam, nihil, nobis quos voluptas repellat cupiditate exercitationem!
+      Corrupti, eveniet.loremlorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Neque alias odio provident? Nostrum voluptate reiciendis
+      ab qui! Ratione, eligendi delectus vel distinctio laboriosam sequi,
+      veritatis minus, excepturi quae molestiae placeat loremloreml loremlorem
+      <br />
+      content here Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Minus rem atque quis esse fugit laudantium sed ut doloremque harum non
+      numquam, nihil, nobis quos voluptas repellat cupiditate exercitationem!
+      Corrupti, eveniet.loremlorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Neque alias odio provident? Nostrum voluptate reiciendis
+      ab qui! Ratione, eligendi delectus vel distinctio laboriosam sequi,
+      veritatis minus, excepturi quae molestiae placeat loremloreml loremlorem
+      <br />
+      content here Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Minus rem atque quis esse fugit laudantium sed ut doloremque harum non
+      numquam, nihil, nobis quos voluptas repellat cupiditate exercitationem!
+      Corrupti, eveniet.loremlorem Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Neque alias odio provident? Nostrum voluptate reiciendis
+      ab qui! Ratione, eligendi delectus vel distinctio laboriosam sequi,
+      veritatis minus, excepturi quae molestiae placeat loremloreml loremlorem
+    </div>
+  );
+};
+
+export default Upload;
