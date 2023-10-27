d3a0f156526a806ab7337438795277ac8ca22d80
Shabin-k <helloshabink@gmail.com>
2023-10-27 15:52:53 +0530

    fix : fixed styeling problem

--- components/admin/ui/Upload.jsx
+++ components/admin/ui/Upload.jsx
@@ -1,13 +1,13 @@
 const Upload = () => {
   return (
-    <div class="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center w-full">
       <label
-        for="dropzone-file"
-        class="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-lg cursor-pointer hover:bg-bray-800 bg-gray-700 border-gray-600 hover:border-gray-500 hover:bg-gray-600"
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-lg cursor-pointer hover:bg-bray-800 bg-gray-700 border-gray-600 hover:border-gray-500 hover:bg-gray-600"
       >
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
-            class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -15,22 +15,22 @@ const Upload = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
           </svg>
-          <p class="mb-2 text-sm text-gray-400 hidden md:flex">
-            <span class="font-semibold">Click to upload</span>&nbsp;or&nbsp;
-            <span class="font-semibold">Drag & Drop files</span>
+          <p className="mb-2 text-sm text-gray-400 hidden md:flex">
+            <span className="font-semibold">Click to upload</span>&nbsp;or&nbsp;
+            <span className="font-semibold">Drag & Drop files</span>
           </p>
           <a className="btn md:hidden flex mb-2 text-white">Browse my files</a>
-          <p class="text-sm md:text-xs text-gray-400">
+          <p className="text-sm md:text-xs text-gray-400">
             Supported file : pdf, doc, docx
           </p>
         </div>
-        <input id="dropzone-file" type="file" class="hidden" />
+        <input id="dropzone-file" type="file" className="hidden" />
       </label>
     </div>
   );
