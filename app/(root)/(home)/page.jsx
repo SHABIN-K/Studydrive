a3b9484d88b655b37d583b321f0736416c373128
Shabin-k <helloshabink@gmail.com>
2023-11-26 12:13:00 +0530

    feat : remove category from menu field

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,21 +1,23 @@
 import Feed from "@/components/Feed";
-import { category, courses } from "@/constants";
+import { courses } from "@/constants";
 
 export default function Home() {
   return (
     <div className="md:flex">
       <div>
+        {/* 
         <Feed
           label="Discover categories"
           feedData={category}
           style="md:flex md:flex-wrap mt-[18px] xs:gap-1.5"
           route="category"
         />
+        */}
         <Feed
-          label="Courses"
+          label="Discover Courses"
           feedData={courses}
           styleHead="mt-3"
-          style="md:grid-cols-5  mt-2 gap-1.5 justify-between md:justify-start"
+          style="md:grid-cols-5  mt-4 gap-1.5 justify-between md:justify-start"
           cardStyle="mt-2 md:mt-0"
           route="courses"
         />
