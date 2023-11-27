c7dd073c9a845c43a527527f37867d7bd0ebc204
Shabin-k <helloshabink@gmail.com>
2023-11-27 12:44:12 +0530

    style(subcard): fixing some style and improved style

--- components/cards/SubCard.jsx
+++ components/cards/SubCard.jsx
@@ -5,12 +5,14 @@ import img from "public/icons/test.png";
 const SubCard = ({ data }) => {
   //https://daisyui.com/components/card/
   return (
-    <div className="rounded-lg bg-[#1c1c24] hover:bg-[#2c2f32] py-2">
-      <div className="flex flex-row card cursor-pointer items-start ml-5">
-        <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
-          <Image src={img} alt={data.name} />
-        </figure>
-        <div className="items-center text-start ml-5">
+    <div className="card rounded-lg cursor-pointer bg-[#1c1c24] hover:bg-[#2c2f32] py-2">
+      <div className="flex flex-row items-center ml-5">
+        <div className="">
+          <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
+            <Image src={img} alt={data.name} />
+          </figure>
+        </div>
+        <div className="text-start ml-5">
           <p className="text-lg font-semibold font-sans md:font-mono  mt-2 text-white">
             {data.name}
           </p>
