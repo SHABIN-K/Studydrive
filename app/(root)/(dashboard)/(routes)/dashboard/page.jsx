951ca5b9335c5ad7e5f2ccd1cfc6b1aabb898630
Shabin-k <helloshabink@gmail.com>
2023-11-29 20:43:58 +0530

    feat : add new feature

--- app/(root)/(dashboard)/(routes)/dashboard/page.jsx
+++ app/(root)/(dashboard)/(routes)/dashboard/page.jsx
@@ -13,7 +13,12 @@ import UploadDoc from "@/components/admin/ui/UploadDoc";
 const MyDash = () => {
   const [activeStep, setActiveStep] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([
+    {
+      name: "pdf-file-for-development.pdf",
+      size: 132424,
+    },
+  ]);
 
   // Array of steps
   const steps = ["UPLOAD", "DETAILS", "DONE"];
@@ -22,9 +27,15 @@ const MyDash = () => {
   const getSectionComponent = () => {
     switch (activeStep) {
       case 0:
-        return <UploadDoc files={files} setFiles={setFiles} removeFile={removeFile}/>;
+        return (
+          <UploadDoc
+            files={files}
+            setFiles={setFiles}
+            removeFile={removeFile}
+          />
+        );
       case 1:
-        return <DocDetails files={files} removeFile={removeFile}/>;
+        return <DocDetails files={files} removeFile={removeFile} />;
       case 2:
         return <UploadDone />;
       default:
@@ -87,8 +98,11 @@ const MyDash = () => {
       {/* Stepper component */}
       <Stepper steps={steps} activeStep={activeStep} />
 
-      {/* Section component based on the active step */}
+      {/* Section component based on the active step 
       <div className="bg-[#1d232a] p-3 rounded-lg mt-5 border border-green-400 w-full max-h-[440px] mb-5 overflow-hidden">
+      </div>*/}
+
+      <div className="bg-[#1d232a] p-3 rounded-lg mt-5 border border-green-400 w-full max-h-[490px] mb-5 overflow-auto">
         {getSectionComponent()}
       </div>
 
--- components/admin/ui/ComboBox.jsx
+++ components/admin/ui/ComboBox.jsx
@@ -2,8 +2,7 @@ import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-const ComboBox = ({ value, onChange, data }) => {
-  //const [selected, setSelected] = useState("Enter course name");
+const ComboBox = ({ value, onChange, data, label, zIndex }) => {
   const [query, setQuery] = useState("");
 
   const filteredCourse =
@@ -17,8 +16,11 @@ const ComboBox = ({ value, onChange, data }) => {
         );
 
   return (
-    <div className="w-full md:w-72">
+    <div className="relative w-full lg:w-72">
       <Combobox value={value} onChange={onChange}>
+        <Combobox.Label className="text-white font-medium md:font-semibold">
+          {label}
+        </Combobox.Label>
         <div className="relative mt-1">
           <div className="relative w-full cursor-text overflow-hidden rounded-lg text-left border-black">
             <Combobox.Input
@@ -40,7 +42,10 @@ const ComboBox = ({ value, onChange, data }) => {
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 sm:text-sm">
+            <Combobox.Options
+              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 sm:text-sm"
+              style={{ zIndex: zIndex }}
+            >
               {filteredCourse.length === 0 && query !== "" ? (
                 <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                   Nothing found.
