811ce7b19cb9e9338903f13f68460f679e125750
Shabin-k <helloshabink@gmail.com>
2023-12-02 00:07:48 +0530

    feat : added Add button in combobox

--- components/admin/ui/ComboBox.jsx
+++ components/admin/ui/ComboBox.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
 
-const ComboBox = ({ value, onChange, data, label, zIndex }) => {
+const ComboBox = ({ value, onChange, data, label, zIndex, subTrue }) => {
   const [query, setQuery] = useState("");
 
   const filteredData =
@@ -22,7 +24,6 @@ const ComboBox = ({ value, onChange, data, label, zIndex }) => {
           {label}
         </Combobox.Label>
         <div className="relative mt-1">
-
           <div className="relative w-full cursor-text overflow-hidden rounded-lg text-left border-black">
             <Combobox.Input
               className="w-full bg-gray-300 py-2 pl-3 pr-10 text-sm text-black font-medium"
@@ -48,7 +49,13 @@ const ComboBox = ({ value, onChange, data, label, zIndex }) => {
               className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 sm:text-sm"
               style={{ zIndex: zIndex }}
             >
-              {filteredData.length === 0 && query !== "" ? (
+              {filteredData.length === 0 && query !== "" && subTrue ? (
+                <Link href="/dashboard">
+                  <div className="relative cursor-pointer select-none px-4 py-2 text-gray-700 hover:text-green-600">
+                    Add "{query}" to list
+                  </div>
+                </Link>
+              ) : filteredData.length === 0 && query !== "" ? (
                 <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                   Nothing found.
                 </div>
@@ -88,7 +95,6 @@ const ComboBox = ({ value, onChange, data, label, zIndex }) => {
               )}
             </Combobox.Options>
           </Transition>
-          
         </div>
       </Combobox>
     </div>
