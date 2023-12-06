327368e96ef397ce75c744d4f501233b9992bdb3
Shabin-k <helloshabink@gmail.com>
2023-12-06 22:50:19 +0530

    refactor: create tabs

---
+++ components/admin/components/ShowSubject.jsx
@@ -0,0 +1,9 @@
+const ShowSubject = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh] ">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default ShowSubject;
