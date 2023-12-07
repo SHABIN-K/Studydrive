ef4eff43f8d3e51e0c1e79c9971bd42da90d7939
Shabin-k <helloshabink@gmail.com>
2023-12-07 21:49:29 +0530

    fix

--- components/admin/components/ChangePassword.jsx
+++ components/admin/components/ChangePassword.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
+import FormButtons from "@/components/ui/FormButtons";
+import FormField from "@/components/ui/FormField";
+import { SmallLoading } from "@/public/assets";
 
 const ChangePassword = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
 
-  const handleOldPasswordChange = (e) => {
-    setOldPassword(e.target.value);
-  };
-
-  const handleNewPasswordChange = (e) => {
-    setNewPassword(e.target.value);
+  const styleChangePwd = {
+    classlabel: "block mb-2 text-sm font-medium text-gray-900",
+    classInput:
+      "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5",
   };
 
   const handleSubmit = (e) => {
@@ -18,50 +21,50 @@ const ChangePassword = () => {
     console.log("Old Password:", oldPassword);
     console.log("New Password:", newPassword);
     // Add your password change logic here
-  
   };
 
   return (
-    <div className="relative w-full max-w-2xl h-full md:h-auto py-6 pl-7 ">
+    <div className="relative w-full max-w-sm h-full md:h-auto">
       <div className="relative p-4 bg-white rounded-lg sm:p-5">
         <div className="items-center pb-4 mb-4 rounded-t border-b sm:mb-5">
           <h3 className="text-lg font-semibold text-gray-900">
-            <a>Change Password</a>
+            <a href="#changepwd">Change Password</a>
           </h3>
         </div>
-        <form onSubmit={handleSubmit}>
+        <div>
           <div className="mb-4">
-            <label htmlFor="oldPassword" className="block text-sm font-medium text-gray-700">
-              Old Password
-            </label>
-            <input
+            <FormField
+              label="Current Password"
               type="password"
-              id="oldPassword"
               name="oldPassword"
               value={oldPassword}
               placeholder="Enter old password"
-              onChange={handleOldPasswordChange}
-              className="mt-1 p-2 w-full border rounded-md text-black"
+              onChange={(e) => setOldPassword(e.target.value)}
+              classLabel={styleChangePwd.classlabel}
+              classInput={`${styleChangePwd.classInput} uppercase`}
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">
-              New Password
-            </label>
-            <input
+            <FormField
+              label="New Password"
               type="password"
-              id="newPassword"
               name="newPassword"
               value={newPassword}
               placeholder="Enter new password"
-              onChange={handleNewPasswordChange}
-              className="mt-1 p-2 w-full border rounded-md text-black"
+              onChange={(e) => setNewPassword(e.target.value)}
+              classLabel={styleChangePwd.classlabel}
+              classInput={`${styleChangePwd.classInput} uppercase`}
             />
           </div>
-          <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
-            Change Password
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            disabled={isLoading}
+            className="text-white inline-flex items-center bg-green-400 hover:bg-green-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            {isLoading ? <SmallLoading /> : "Change password"}
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
