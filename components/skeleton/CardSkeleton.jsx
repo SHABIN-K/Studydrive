93cffa2bf264b5fea9ee1217db2b1375a29d0b8a
Shabin-k <helloshabink@gmail.com>
2023-12-16 11:20:59 +0530

    style: improve style

--- components/skeleton/CardSkeleton.jsx
+++ components/skeleton/CardSkeleton.jsx
@@ -1,6 +1,6 @@
 const CardSkeleton = () => {
   return (
-    <div className="grid items-center gap-2 rounded-2xl bg-[#1c1c24] p-4 shadow-2xl hover:shadow-[#272433] md:grid-cols-[1fr_auto]">
+    <div className="grid items-center gap-2 rounded-2xl bg-[#1c1c24] p-4 md:grid-cols-[1fr_auto]">
       <div className="grid grid-cols-[auto_1fr] grid-rows-[3,auto] items-center gap-x-4 gap-y-2">
         <div className="skeleton aspect-square h-full min-w-[50px] overflow-hidden rounded-full object-contain md:row-span-3 md:h-20" />
         <div />
