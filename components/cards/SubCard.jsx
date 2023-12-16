4724858585ac0f8f0ff52561b2629f87751a69e2
Shabin-k <helloshabink@gmail.com>
2023-12-16 23:05:32 +0530

    refactor : change loadin main page

--- components/cards/SubCard.jsx
+++ components/cards/SubCard.jsx
@@ -1,50 +1,36 @@
 "use client";
+import img from "public/icons/doc.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-import img from "public/icons/doc.png";
-import CardSkeleton from "../skeleton/CardSkeleton";
-
-const SubCard = ({ data, isLoading }) => {
+const SubCard = ({ data }) => {
   const router = useRouter();
 
   const handleClick = () => {
     router.push("/view-doc");
   };
 
-  const skeleton = [...Array(1).keys()].map((i) => {
-    return <CardSkeleton key={i} />;
-  });
-
   return (
-    <>
-      {isLoading ? (
-        <>
-          <ul className="grid gap-6">{skeleton}</ul>
-        </>
-      ) : (
-        <div
-          className="card rounded-lg cursor-pointer bg-[#1c1c24] hover:bg-[#2c2f32] py-2"
-          onClick={handleClick}
-        >
-          <div className="flex flex-row items-center ml-5">
-            <div className="">
-              <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
-                <Image src={img} alt={data.subject_name} />
-              </figure>
-            </div>
-            <div className="text-start ml-5">
-              <p className="text-lg font-semibold font-sans md:font-mono  mt-2 text-white">
-                {data.subject_name}
-              </p>
-              <p className="text-base font-medium md:font-semibold font-sans md:font-mono tracking-tighter  text-[#808191]">
-                code : <span className="uppercase">{data.subject_code}</span>
-              </p>
-            </div>
-          </div>
+    <div
+      className="card rounded-lg cursor-pointer bg-[#1c1c24] hover:bg-[#2c2f32] py-2"
+      onClick={handleClick}
+    >
+      <div className="flex flex-row items-center ml-5">
+        <div className="">
+          <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
+            <Image src={img} alt={data.subject_name} />
+          </figure>
         </div>
-      )}
-    </>
+        <div className="text-start ml-5">
+          <p className="text-lg font-semibold font-sans md:font-mono  mt-2 text-white">
+            {data.subject_name}
+          </p>
+          <p className="text-base font-medium md:font-semibold font-sans md:font-mono tracking-tighter  text-[#808191]">
+            code : <span className="uppercase">{data.subject_code}</span>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
