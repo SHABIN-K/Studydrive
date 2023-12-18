abe72464113fad67fb30799aa9e6533584dff4ea
Shabin-k <helloshabink@gmail.com>
2023-12-18 21:42:16 +0530

    refactor : improve performance

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,27 +1,24 @@
 import Feed from "@/components/Feed";
-import { courses } from "@/constants";
 
 export default function Home() {
   return (
     <div className="md:flex">
       <div>
-        {/* 
+        <Feed
+          label="Discover Courses"
+          styleHead="mt-3"
+          style="md:grid-cols-5  mt-4 gap-1.5 justify-between md:justify-start"
+        />
+      </div>
+    </div>
+  );
+}
+
+/* 
         <Feed
           label="Discover categories"
           feedData={category}
           style="md:flex md:flex-wrap mt-[18px] xs:gap-1.5"
           route="category"
         />
-        */}
-        <Feed
-          label="Discover Courses"
-          feedData={courses}
-          styleHead="mt-3"
-          style="md:grid-cols-5  mt-4 gap-1.5 justify-between md:justify-start"
-          cardStyle="mt-2 md:mt-0"
-          route="category"
-        />
-      </div>
-    </div>
-  );
-}
+*/
