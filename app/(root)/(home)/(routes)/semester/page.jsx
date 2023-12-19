6b45911c07a170374d41dc9e5b313a70f90861c1
Shabin-k <helloshabink@gmail.com>
2023-12-19 18:52:08 +0530

    feat : add new feature

--- app/(root)/(home)/(routes)/semester/page.jsx
+++ app/(root)/(home)/(routes)/semester/page.jsx
@@ -14,34 +14,46 @@ const UserSemester = () => {
   const course = searchParams.get("name");
   const category = searchParams.get("category");
 
+  const fetchedData = usePostStore((state) => state.posts);
+
   const [isPostOpen, setIsPostOpen] = useState(false);
   const [data, setData] = useState(null);
 
-  if (category === "Videos") {
-    return <NoDataFound />;
-  }
-
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    if (category === "Syllabus") {
-      const fetchedData = usePostStore((state) => state.posts);
-      const post = filterSyllabus([course, category], fetchedData);
-      const [syllabusData] = post.map((items) => items);
+    const fetchData = async () => {
+      try {
+        const post = await filterSyllabus([course, category], fetchedData);
+        const [syllabusData] = post.map((items) => items);
 
-      // Set the state to make the dialog box open and pass data
-      if (syllabusData) {
-        setIsPostOpen(true);
-        setData(syllabusData);
+        if (syllabusData) {
+          setIsPostOpen(true);
+          setData(syllabusData);
+        } else {
+          // Handle case where data is not found
+          setIsPostOpen(false);
+          setData(null);
+        }
+      } catch (error) {
+        // Handle error
+        console.error("Error fetching syllabus data:", error);
       }
+    };
+
+    if (category === "Syllabus") {
+      fetchData();
     }
-  }, [category, course]);
+  }, [category, course, fetchedData]);
 
   return (
     <div>
       <h1 className="select_header">Select Semester</h1>
       <div className="items-center">
-        <div className="grid grid-cols-2 mt-[18px] gap-[18px]">
-          {semester.map((sem, index) => {
-            return (
+        {category === "Videos" || category === "Syllabus" ? (
+          <NoDataFound />
+        ) : (
+          <div className="grid grid-cols-2 mt-[18px] gap-[18px]">
+            {semester.map((sem, index) => (
               <DataCard
                 key={index}
                 hrefData={{
@@ -49,14 +61,14 @@ const UserSemester = () => {
                   query: { name: course, category: category, sem: sem.link },
                 }}
                 data={sem}
-                altMsg="select you semester"
+                altMsg="select your semester"
                 style="bg-[#1c1c24] hover:bg-[#2c2f32] py-2"
                 syleName="text-white"
                 sem="Semester"
               />
-            );
-          })}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
       {isPostOpen && data && (
         <PostViewDialogBox
