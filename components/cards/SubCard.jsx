2a97a1ba383c51562757a2a281ae457012fad5ed
Shabin-k <helloshabink@gmail.com>
2023-12-19 19:26:46 +0530

    fix : alignment bug 🐛

--- components/cards/SubCard.jsx
+++ components/cards/SubCard.jsx
@@ -10,10 +10,11 @@ const SubCard = ({ hrefData, data }) => {
       className="card rounded-lg cursor-pointer bg-[#1c1c24] hover:bg-[#2c2f32] py-2"
     >
       <div className="flex flex-row items-center ml-5">
-        <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
-          <Image src={subject} alt={data.subject_name} />
-        </figure>
-
+        <div>
+          <figure className="w-16 h-16 md:w-20 md:h-20 bg-gray-300 rounded-full overflow-hidden">
+            <Image src={subject} alt={data.subject_name} />
+          </figure>
+        </div>
         <div className="text-start ml-5">
           <p className="text-lg font-semibold font-sans md:font-mono  mt-2 text-white">
             {data.subject_name}
