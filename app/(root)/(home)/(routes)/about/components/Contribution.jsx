f3d39782678b4d1c7431b9aecfca36aa4ac507ab
Shabin-k <helloshabink@gmail.com>
2024-01-05 23:28:57 +0530

    feat : add contribute

--- app/(root)/(home)/(routes)/about/components/Contribution.jsx
+++ app/(root)/(home)/(routes)/about/components/Contribution.jsx
@@ -1,7 +1,42 @@
-import React from "react";
-
+import Image from "next/image";
+import Link from "next/link";
+import { IconBrandGithub } from "@tabler/icons-react";
 const Contribution = () => {
-  return <div>Contribution</div>;
+  const contributeText = {
+    title: "Paschub is an open source project built with 💖",
+    desc: "Open-source, passion-fueled project. Revolutionizing education with an intuitive platform for seamless student-teacher interaction.",
+    subDesc:
+      "We highly appreciate any feedback or contribution that could help us improve.",
+  };
+  return (
+    <section className="w-full mt-3">
+      <div className="flex justify-center items-center flex-col-reverse md:flex-row">
+        <div className="">
+          <Image
+            src="/contriubute.svg"
+            alt="github contribution svg"
+            width={350}
+            height={350}
+          />
+        </div>
+        <div className="items-center text-center text-lg font-normal sm:font-medium w-full md:w-[60%] text-[#808191]">
+          <h3 className="md:text-2xl font-bold text-white mb-2">
+            {contributeText.title}
+          </h3>
+          <p>{contributeText.desc}</p>
+          <p className="mt-2">{contributeText.subDesc}</p>
+          <Link
+            href="https://github.com/SHABIN-K/Studydrive"
+            target="_blank"
+            className="flex hover:text-white items-center justify-center mt-3 text-2xl gap-1 text-gray-400"
+          >
+            <IconBrandGithub size={30} />
+            <p>Github</p>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contribution;
--- app/(root)/(home)/(routes)/about/components/Hero.jsx
+++ app/(root)/(home)/(routes)/about/components/Hero.jsx
@@ -1,4 +1,3 @@
-
 const Hero = () => {
   const text = {
     tittle_one: "Dive into the Sea of ",
@@ -8,12 +7,12 @@ const Hero = () => {
   };
   return (
     <section className="text-white">
-      <div className="mx-auto max-w-3xl text-center mt-10 items-center justify-center">
+      <div className="mx-auto text-center mt-10 items-center justify-center max-w-3xl">
         <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-5xl font-extrabold text-transparent sm:text-5xl md:text-7xl">
           {text.tittle_one}
           <span className="sm:block">{text.tittle_two}</span>
         </h1>
-        <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+        <p className="mx-auto mt-4 max-w-xl sm:text-xl font-medium">
           {text.descrption}
         </p>
       </div>
